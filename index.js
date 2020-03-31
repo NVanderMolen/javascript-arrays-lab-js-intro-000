48c6c273ee18908a6e5ed667f8adaa9251258061
Nick VanderMolen <tree.vandermolen@gmail.com>
2020-03-31 00:54:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function appendKitten(name){
   return moreKittens;
 }
 function prependKitten(name){
-  var morekittens = kittens.slice().unshift(name);
+  var morekittens = kittens.slice(0,1);
   return moreKittens;
 }
 function removeLastKitten(){
